ba5d6b47426a2253be981e45160fba9f262bd13c
sv9tr <74360327+sv9tr@users.noreply.github.com>
2020-11-14 19:00:38 +0200

    doar daca-s ceva probleme am scris ce am modificat
    La Categorii doar cand apesi pe Ingrijire par si Ingrijire pers--Sampon balsam apar produse pe pagina ca am facut <div class="container" si i-am dat sa apara/dispara cand dau click pe categorie
    -Machiaj, Ingrijire personala  se pot restrange/extinde
    -3 scripturi la final
    -2 randuri in JS
    -o poza in img
    -css=> btnn e pt butoanele de la cumparat sampon

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
-$(document).ready(function() {
-    $("#register").on("click", function (event) {
-        event.preventDefault();
-        $(".container2").empty();
-        $(".container2").load("register.html");
+https: //maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js
+    https: //cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js
+    $(document).ready(function() {
+        $("#register").on("click", function(event) {
+            event.preventDefault();
+            $(".container2").empty();
+            $(".container2").load("register.html");
 
-    })
-    $("#home").on("click", function (event) {
-        event.preventDefault();
-        $(".container2").empty();
-        $("body").load("index.html");
+        })
+        $("#home").on("click", function(event) {
+            event.preventDefault();
+            $(".container2").empty();
+            $("body").load("index.html");
 
-    })
-});
+        })
+    });
