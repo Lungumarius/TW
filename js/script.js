792070a214ad5e8c473389f32edb2408a2a46ad6
Lungu Marius <65728389+Lungumarius@users.noreply.github.com>
2020-11-17 09:16:09 +0200

    Register / Login

--- js/script.js
+++ js/script.js
@@ -1,10 +1,8 @@
-https: //maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js
-    https: //cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js
     $(document).ready(function() {
         $("#register").on("click", function(event) {
             event.preventDefault();
             $(".container2").empty();
-            $(".container2").load("register.html");
+            $(".container2").load("../html/register.html");
 
         })
         $("#home").on("click", function(event) {
