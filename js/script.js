c200c42f4ed2951f1058ba6e361f3f1e97984dd0
Lungu Marius <65728389+Lungumarius@users.noreply.github.com>
2020-11-17 16:28:04 +0200

    Fix 2

--- js/script.js
+++ js/script.js
@@ -11,4 +11,27 @@
             $("body").load("index.html");
 
         })
+        $('.dropdown-submenu a.test').on("click", function(e) {
+            $(this).next('ul').toggle();
+            e.stopPropagation();
+            e.preventDefault();
+        });
+        function myFunction() {
+            var x = document.getElementById("displayPar");
+            if (x.style.display === "none") {
+                x.style.display = "block";
+            } else {
+                x.style.display = "none";
+            }
+        }
+        function BlockPar() {
+            var x = document.getElementById("displayPar");
+            x.style.display = "none";
+        }
+        $("#ingrijire_par").on("click", function(event) {
+            event.preventDefault();
+            $(".container2").empty();
+            $(".container2").load("../html/ingrijire_par.html");
+
+        })
     });
